378f8cc878274d54ec6629bb8b66bca7baa8e7b0
Arrowana <8245419+Arrowana@users.noreply.github.com>
2022-03-29 12:46:13 +1100

    Remove commented code, drop package

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,9 @@
 import { Keypair, PublicKey } from "@solana/web3.js";
-import bs58 from "bs58";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 export const RPC_ENDPOINT = "https://ssc-dao.genesysgo.net";
-
-// export const WALLET_BASE58_PRIVATE_KEY =
-//   process.env.WALLET_BASE58_PRIVATE_KEY || "PASTE YOUR WALLET PRIVATE KEY";
-// export const USER_PRIVATE_KEY = bs58.decode(WALLET_BASE58_PRIVATE_KEY);
-// export const USER_KEYPAIR = Keypair.fromSecretKey(USER_PRIVATE_KEY);
 
 export const USDC_MINT = new PublicKey(
   "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
