c166ee4fcceddfbda23eda26cb3427f2606798e0
Arrowana <8245419+Arrowana@users.noreply.github.com>
2022-03-29 12:45:12 +1100

    feat: Keypair from file rather than .env

--- src/constants.ts
+++ src/constants.ts
@@ -6,10 +6,10 @@ dotenv.config();
 
 export const RPC_ENDPOINT = "https://ssc-dao.genesysgo.net";
 
-export const WALLET_BASE58_PRIVATE_KEY =
-  process.env.WALLET_BASE58_PRIVATE_KEY || "PASTE YOUR WALLET PRIVATE KEY";
-export const USER_PRIVATE_KEY = bs58.decode(WALLET_BASE58_PRIVATE_KEY);
-export const USER_KEYPAIR = Keypair.fromSecretKey(USER_PRIVATE_KEY);
+// export const WALLET_BASE58_PRIVATE_KEY =
+//   process.env.WALLET_BASE58_PRIVATE_KEY || "PASTE YOUR WALLET PRIVATE KEY";
+// export const USER_PRIVATE_KEY = bs58.decode(WALLET_BASE58_PRIVATE_KEY);
+// export const USER_KEYPAIR = Keypair.fromSecretKey(USER_PRIVATE_KEY);
 
 export const USDC_MINT = new PublicKey(
   "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
