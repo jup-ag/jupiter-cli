ccc75f43fb23aad59cad8c2b0a9339806f273666
wentokay <wentokay@users.noreply.github.com>
2023-01-09 11:04:46 +0000

    add fallback RPC_NODE_URL value

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,9 @@ import { PublicKey } from "@solana/web3.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const RPC_NODE_URL = process.env.RPC_NODE_URL as string;
+export const RPC_NODE_URL = String(
+  process.env.RPC_NODE_URL || "https://api.mainnet-beta.solana.com"
+);
 
 export const USDC_MINT = new PublicKey(
   "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
